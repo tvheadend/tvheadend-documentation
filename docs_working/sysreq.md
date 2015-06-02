7e30aca4d575eacae60f6abc73bba9826fd8aea8
ProfYaffle <ian@sproink.co.uk>
2015-06-02 13:55:59 +0100

    Updated ignored files & add simple resync utility

--- docs_working/sysreq.md
+++
@@ -1,9 +0,0 @@
-<div class="hts-doc-text">
-
-If you want to build Tvheadend from source, please visit
-[this](https://tvheadend.org/projects/tvheadend/wiki/Building) page.
-Please note: that wiki development site only reflects the work in
-master. It should not deviate much should you want to build a released
-version from source, but you never know.
-
-</div>
---
+++ resync.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Simple utility to automate the conversion of markdown docs to a 
+# standalone web site, and then push that website to github pages.
+#
+# Note that all directory paths are hard-coded. Sue me, it's my computer
+# and it works here (okay, it's not perfect, I know - "WIBNI"...
+
+# Rebuild html
+cd ~/tvh/tvheadend-documentation
+mkdocs build --clean
+
+# Copy it to local github pages repository
+cd ~/tvh/tvheadend.github.io
+cp -r ~/tvh/tvheadend-documentation/site/* .
+
+# Commit it and push
+git add -A
+git commit -m "Resync with source repository"
+git push
